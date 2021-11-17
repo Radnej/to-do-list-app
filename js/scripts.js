05917b11a862bea011f26be82bd20c35761a4eb5
Radnej <snejatiun@gmail.com>
2021-11-17 15:58:25 +0100

    Update scripts.js
    Edit code via Auto Indent
    Edit/scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,9 @@
 
 function newItem(){
 
-//javascript
-//1. Adding a new item to the list of items: 
-   let listItem = $('<li></li>');
+  //javascript
+  //1. Adding a new item to the list of items:
+  let listItem = $('<li></li>');
   let inputValue = $('#input').val()
   listItem.append(inputValue);
 
@@ -13,26 +13,24 @@ function newItem(){
     $('#list').append(listItem)
   }
 
- //2. Crossing out an item from the list of items:
-
-  
+  //2. Crossing out an item from the list of items:
 
   listItem.on('dblclick', function () {
-  listItem.toggleClass('strike');
-});
+    listItem.toggleClass('strike');
+  });
 
- //3(i). Adding the delete button "X": 
+  //3(i). Adding the delete button "X":
 
-   let deleteButton = $('<button></button>');
+  let deleteButton = $('<button></button>');
   deleteButton.append(document.createTextNode('X'));
   listItem.append(deleteButton);
 
   deleteButton.on("click", deleteListItem);
 
- //3(ii). Adding CLASS DELETE (DISPLAY: NONE) from the css:
-   function deleteListItem() {
+  //3(ii). Adding CLASS DELETE (DISPLAY: NONE) from the css:
+  function deleteListItem() {
     listItem.addClass("delete")
   }
- // 4. Reordering the items: 
-   $('#list').sortable();
+  // 4. Reordering the items:
+  $('#list').sortable();
 }
